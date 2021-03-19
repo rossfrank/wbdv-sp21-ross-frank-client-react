4210368d4458ae1c49ad9ddf469e31a9e4191a11
rossfrank <rossfrank27@gmail.com>
2021-03-19 16:18:35 -0400

    add widget service

---
+++ src/services/widget-service.js
@@ -0,0 +1,43 @@
+const WIDGET_URL = "http://localhost:8080/api";
+
+export const createWidget = (tid, widget) =>
+    fetch(`${WIDGET_URL}/topics/${tid}/widgets`, {
+        method: "POST",
+        body: JSON.stringify(widget),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+export const findWidgetsForTopic = (tid) =>
+    fetch(`${WIDGET_URL}/topics/${tid}/widgets`)
+        .then(response => response.json())
+
+export const findAllWidgets = () =>
+    fetch(`${WIDGET_URL}/widgets`)
+        .then(response => response.json())
+
+export const findWidgetById = (wid) =>
+    fetch(`${WIDGET_URL}/widgets/${wid}`)
+        .then(response => response.json())
+
+export const updateWidget = (wid, widget) =>
+    fetch(`${WIDGET_URL}/widgets/${wid}`, {
+        method: "PUT",
+        body: JSON.stringify(widget),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+export const deleteWidget= (wid) =>
+    fetch(`${WIDGET_URL}/widgets/${wid}`, {
+        method: "DELETE"
+    })
+        .then(response => response.json())
+
+export default {
+    createWidget, findWidgetById, findWidgetsForTopic, findAllWidgets, updateWidget, deleteWidget
+}
